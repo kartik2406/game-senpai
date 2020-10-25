face43d91c13d240bf25cdcd61984d1df767b9d3
Kartik Malik <kartikmalik91@gmail.com>
2020-10-25 18:34:52 +0530

    Adding basic discord API

--- functions/epicUpdates.js
+++ functions/epicUpdates.js
@@ -1,7 +1,24 @@
-exports.handler = async event => {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: `Hello ${subject}!`,
-    }
+//@ts-check
+const utils = require("../utils");
+const channelID = process.env.channelID; // TODO: To be read from config
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const token = process.env.TOKEN;
+
+bot.login(token);
+
+exports.handler = async (event) => {
+  let games = await utils.getWeeklyFreeEpicGames();
+
+  if (games) {
+    const channel = bot.channels.cache.get(channelID);
+
+    games.forEach((game) => channel.send(utils.getFreeGamesEmbed(game)));
   }
+  const subject = event.queryStringParameters.name || "World";
+  return {
+    statusCode: 200,
+    body: `Hello ${subject}!`,
+  };
+};
