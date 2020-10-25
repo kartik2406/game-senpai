862ceabad3f73baad5b80f8d6d33f66ce2a1c94b
Kartik Malik <kartikmalik91@gmail.com>
2020-10-25 23:02:50 +0530

    Trying vercel

--- api/fetchEpic.js
+++ api/fetchEpic.js
@@ -1,9 +1,42 @@
-module.exports = (req, res) => {
+const utils = require("../utils");
+const channelID = process.env.channelID; // TODO: To be read from config
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const token = process.env.TOKEN;
+
+bot.login(token);
+
+const getChannel = (channelID) => {
+  return new Promise((resolve, reject) => {
+    bot.on("ready", () => {
+      const channel = bot.channels.fetch(channelID);
+      resolve(channel);
+    });
+  });
+};
+module.exports = async (req, res) => {
+  let games = await utils.getWeeklyFreeEpicGames();
+
+  if (games) {
+    const channel = await getChannel(channelID);
+    let messagePromises = games.map((game) =>
+      channel.send(utils.getFreeGamesEmbed(game))
+    );
+
+    await Promise.all(messagePromises);
+
+    res.json({
+      status: 200,
+      body: {
+        message: "hello world",
+      },
+    });
+  }
   res.json({
+    status: 400,
     body: {
-      message: "hello world",
+      message: "No games found",
     },
-    query: req.query,
-    cookies: req.cookies,
   });
 };
