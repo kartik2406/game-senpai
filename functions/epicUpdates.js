bdbdf2a4ddee73f8bb4e8dba08ab8829a35dd638
Kartik Malik <kartikmalik91@gmail.com>
2020-10-25 19:30:28 +0530

    Fix for not able to find channel

--- functions/epicUpdates.js
+++ functions/epicUpdates.js
@@ -2,19 +2,31 @@
 const utils = require("./utils");
 const channelID = process.env.channelID; // TODO: To be read from config
 const Discord = require("discord.js");
+const { reject } = require("lodash");
 const bot = new Discord.Client();
 
 const token = process.env.TOKEN;
 
 bot.login(token);
 
+bot.on;
+
+const getChannel = (channelID) => {
+  return new Promise((resolve, reject) => {
+    bot.on("ready", () => {
+      const channel = bot.channels.fetch(channelID);
+      resolve(channel);
+    });
+  });
+};
 exports.handler = async (event) => {
   let games = await utils.getWeeklyFreeEpicGames();
 
   if (games) {
-    const channel = bot.channels.cache.get(channelID);
-
-    games.forEach((game) => channel.send(utils.getFreeGamesEmbed(game)));
+    const channel = await getChannel(channelID);
+    channel.games.forEach((game) =>
+      channel.send(utils.getFreeGamesEmbed(game))
+    );
   }
   const subject = event.queryStringParameters.name || "World";
   return {
