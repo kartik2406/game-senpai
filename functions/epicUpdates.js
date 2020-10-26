ba3bf5d3c1fcbf561e55078d271833d57f1c0d4a
Kartik Malik <kartikmalik91@gmail.com>
2020-10-26 12:00:00 +0530

    Trying to move login code outside

--- functions/epicUpdates.js
+++ functions/epicUpdates.js
@@ -5,17 +5,18 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 const token = process.env.TOKEN;
+bot.login(token);
 
 const getChannel = (channelID) => {
   return new Promise((resolve, reject) => {
     bot.on("ready", () => {
+      console.log('Bot is ready!')
       const channel = bot.channels.fetch(channelID);
       resolve(channel);
     });
   });
 };
 exports.handler = async (event) => {
-  bot.login(token);
   let games = await utils.getWeeklyFreeEpicGames();
   console.log("Games received", games);
   if (games) {
@@ -26,15 +27,11 @@ exports.handler = async (event) => {
 
     await Promise.all(messagePromises);
     console.log("Messages sent");
-    bot.destroy();
-    console.log("Bot logged out!");
     return {
       statusCode: 200,
       body: `Games found`,
     };
   }
-  bot.destroy();
-  console.log("Bot logged out!");
   return {
     statusCode: 400,
     body: `No games found`,
