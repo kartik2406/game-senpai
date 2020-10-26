100ccedf46d010f85a8801404d02465a87bba133
Kartik Malik <kartikmalik91@gmail.com>
2020-10-26 11:40:05 +0530

    Adding req time interceptor

--- utils.js
+++ utils.js
@@ -1,9 +1,38 @@
-const axios = require("axios");
+const axios = require("axios").default;
 const constants = require("./constants");
 const Discord = require("discord.js");
 const format = require("date-fns/format");
 const { random } = require("lodash");
 const { COMMMAND_RESPONSES } = require("./constants");
+
+axios.interceptors.request.use((x) => {
+  // to avoid overwriting if another interceptor
+  // already defined the same object (meta)
+  x.meta = x.meta || {};
+  x.meta.requestStartedAt = new Date().getTime();
+  return x;
+});
+
+axios.interceptors.response.use(
+  (x) => {
+    console.log(
+      `Execution time for: ${x.config.url} - ${
+        new Date().getTime() - x.config.meta.requestStartedAt
+      } ms`
+    );
+    return x;
+  },
+  // Handle 4xx & 5xx responses
+  (x) => {
+    console.error(
+      `Execution time for: ${x.config.url} - ${
+        new Date().getTime() - x.config.meta.requestStartedAt
+      } ms`
+    );
+    throw x;
+  }
+);
+
 const getWeeklyFreeEpicGames = async () => {
   try {
     let response = await axios(constants.APIS.EPIC_STORES_WEEKLY_FREE_GAMES);
